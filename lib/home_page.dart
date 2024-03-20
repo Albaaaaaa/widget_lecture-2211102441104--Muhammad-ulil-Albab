7ed3716f34766090b25c1d987b74962c3abf2aef
Muhammad ulil Albab <114996165+Albaaaaaa@users.noreply.github.com>
2024-03-20 15:50:21 +0800

    Update AspectRatio

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -127,6 +127,16 @@ class HomePage extends StatelessWidget {
               ),
             ),
 
+            const SizedBox(height: 20.0),
+            const Text(
+              '10. AspectRatio:',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Container(color: Color.fromARGB(255, 10, 230, 255)),
+            ),
+
             
 
             
